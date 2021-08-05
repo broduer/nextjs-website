a564ef9ec97b4765d57acfc4809713e72773acad
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-08-05 13:25:44 -0400

    Added Most Popular and Recently Added

--- areas/miscellaneous/scenes/Discover/components/GameCardsManager/services/getGameCardsData.js
+++ areas/miscellaneous/scenes/Discover/components/GameCardsManager/services/getGameCardsData.js
@@ -1,13 +1,5 @@
-import { BACKEND_SERVER_URL } from "../../../../../../../services/utilities/constants";
-
-export default async function getGameCardsData(gameCardsData) {
-  const response = await (gameCardsData.length === 0
-    ? fetch(`${BACKEND_SERVER_URL}/api/games/verified/page`)
-    : fetch(
-        `${BACKEND_SERVER_URL}/api/games/verified/page/${
-          gameCardsData[gameCardsData.length - 1]._id
-        }`
-      ));
+export default async function getGameCardsData(gameCardsUrl) {
+  const response = await fetch(gameCardsUrl);
 
   const data = await response.json();
 
