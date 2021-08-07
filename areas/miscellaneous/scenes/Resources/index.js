d415508eb0c60967698c6596155cda22c1d64bee
Hasnain Raza <hasnain.mohsan@gmail.com>
2021-08-06 21:09:14 -0400

    Added some heads and favicon

--- areas/miscellaneous/scenes/Resources/index.js
+++ areas/miscellaneous/scenes/Resources/index.js
@@ -4,6 +4,13 @@ import ResourceCard from "./components/ResourceCard";
 export default function Resources() {
   return (
     <div className="w-full min-h-screen flex flex-col bg-nanobloxGray-light">
+      <Head>
+        <title>Nanoblox</title>
+        <meta
+          name="description"
+          content="Find resources and documentations to learn what Nanoblox has to offer to developers and users."
+        />
+      </Head>
       <NavigationBar />
       <div className="container mx-auto my-2 flex-grow">
         <div className="px-4 pt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
